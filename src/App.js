760193acd85b77be50363376a53276dccc404b64
farshid manoochehri <farshid.manoochehri.k@gmail.com>
2020-07-20 12:06:33 +0430

    first step completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Start from "./components/Start";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  const [step, setStep] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      {step === 0 ? <Start onStart={() => setStep(1)} /> : null}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Start.jsx
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  startSection: {
+    border: "solid red 2px",
+    borderRadius: "5px",
+    width: "100%",
+    height: "100%",
+    padding: "5px",
+    maxWidth: "800px",
+    // maxHeight: "400px",
+    backgroundColor: "antiquewhite",
+    display: "flex",
+    flexDirection: "column",
+  },
+  playersNo: {
+    minWidth: "200px",
+  },
+  playersName: {
+    margin: "10px 0px",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  textField: {
+    minWidth: "185px",
+    margin: "5px",
+  },
+  charactersSection: {
+    margin: "10px",
+    border: "2px solid #a2a2a2",
+    borderRadius: "10px",
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  },
+  badCharacter: {
+    minWidth: "100px",
+    margin: "5px",
+    backgroundColor: "#aa303099",
+    border: "1px solid red",
+    color: "black",
+    textAlign: "center",
+  },
+  goodCharacter: {
+    minWidth: "100px",
+    margin: "5px",
+    backgroundColor: "#2020aaaa",
+    border: "1px solid blue",
+    color: "white",
+    textAlign: "center",
+  },
+}));
+
+const Start = (props) => {
+  const classes = useStyles();
+  const [playersNo, setPlayersNo] = useState(5);
+  const [playersName, setPlayersName] = useState([]);
+
+  function handlePlayersNameChange(name, index) {
+    let newArr = [];
+    for (let i = 0; i < playersNo; i++) {
+      if (i === index) {
+        newArr[i] = name;
+      } else {
+        newArr[i] = playersName[i];
+      }
+    }
+    setPlayersName(newArr);
+  }
+
+  useEffect(() => {
+    let newArr = [];
+    for (let i = 0; i < playersNo; i++) {
+      newArr[i] = `player ${i + 1}`;
+    }
+    setPlayersName(newArr);
+  }, [playersNo]);
+
+  return (
+    <div className={classes.startSection}>
+      <Typography variant="h3" align="center">
+        Avalon
+      </Typography>
+      <FormControl className={classes.playersNo}>
+        <InputLabel id="player-number">Player Number</InputLabel>
+        <Select
+          labelId="player-number"
+          id="player-number-select"
+          value={playersNo}
+          onChange={(e) => setPlayersNo(e.target.value)}
+        >
+          {[5, 6, 7, 8, 9, 10].map((number) => {
+            return (
+              <MenuItem key={number} value={number}>
+                {number}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+      <div className={classes.playersName}>
+        {[...Array(playersNo).keys()].map((pn, index) => {
+          return (
+            <TextField
+              className={classes.textField}
+              key={pn}
+              id={`player${pn + 1}-name`}
+              label={`player ${pn + 1}`}
+              variant="outlined"
+              value={playersName[index]}
+              onChange={(e) => {
+                handlePlayersNameChange(e.target.value, index);
+              }}
+            />
+          );
+        })}
+      </div>
+      <div className={classes.charactersSection}>
+        <ShowCharacters playersNo={playersNo} />
+      </div>
+      <Button onClick={props.onStart} variant="contained" color="primary">
+        Start
+      </Button>
+    </div>
+  );
+};
+
+export default Start;
+
+const ShowCharacters = (props) => {
+  const classes = useStyles();
+  const ROYAL_SERVANT = { name: "loyal servant", isBad: false };
+  const CHARACTERS = [
+    ROYAL_SERVANT,
+    { name: "Merlin", isBad: false },
+    { name: "Percival", isBad: false },
+    { name: "Morgana", isBad: true },
+    { name: "Assassin", isBad: true },
+    ROYAL_SERVANT,
+    { name: "Mordred", isBad: true },
+    ROYAL_SERVANT,
+    ROYAL_SERVANT,
+    { name: "Oberon", isBad: true },
+  ];
+
+  return (
+    <>
+      {CHARACTERS.slice(0, props.playersNo).map((ch, index) => (
+        <Character key={index} name={ch.name} isBad={ch.isBad} />
+      ))}
+    </>
+  );
+};
+const Character = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div
+      className={clsx({
+        [classes.badCharacter]: props.isBad,
+        [classes.goodCharacter]: !props.isBad,
+      })}
+    >
+      <h5>{props.name}</h5>
+    </div>
+  );
+};
