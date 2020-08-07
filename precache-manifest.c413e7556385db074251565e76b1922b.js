e01080904c7915d54a5fe2396f09b84dbb8e8c0e
farshid manoochehri <farshid.manoochehri.k@gmail.com>
2020-08-07 18:33:44 +0430

    Updates

---
+++ precache-manifest.c413e7556385db074251565e76b1922b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4f2b2749d737ec7100574abfb37a0b",
+    "url": "/avalon/index.html"
+  },
+  {
+    "revision": "9ed289fa992f12bbb693",
+    "url": "/avalon/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1a289dfb6488f5a86f3a",
+    "url": "/avalon/static/js/2.dc10abb9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/avalon/static/js/2.dc10abb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed289fa992f12bbb693",
+    "url": "/avalon/static/js/main.1016ea64.chunk.js"
+  },
+  {
+    "revision": "358114e393aea6e1ba4a",
+    "url": "/avalon/static/js/runtime-main.c936b9bf.js"
+  }
+]);
