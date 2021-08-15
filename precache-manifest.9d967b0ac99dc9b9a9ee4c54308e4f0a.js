20f3369a9c3a8dd15c2147e0273d2c8220d5777c
=Farshid Manoochehri <farshid.manoochehri.k@gmail.com>
2021-08-15 18:44:02 +0430

    Updates

---
+++ precache-manifest.9d967b0ac99dc9b9a9ee4c54308e4f0a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57955cb45a188eba9a78d714bf4656f4",
+    "url": "/avalon/index.html"
+  },
+  {
+    "revision": "fe5c9f7da76e1a160bb5",
+    "url": "/avalon/static/css/main.5ee1f887.chunk.css"
+  },
+  {
+    "revision": "258be25ade66e68c1ba0",
+    "url": "/avalon/static/js/2.aea845ba.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/avalon/static/js/2.aea845ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5c9f7da76e1a160bb5",
+    "url": "/avalon/static/js/main.b8b199af.chunk.js"
+  },
+  {
+    "revision": "358114e393aea6e1ba4a",
+    "url": "/avalon/static/js/runtime-main.c936b9bf.js"
+  },
+  {
+    "revision": "26f005f7cab6c5bd2cdb8f1da740e1b9",
+    "url": "/avalon/static/media/assassin.26f005f7.png"
+  },
+  {
+    "revision": "1f69d8ac2c5a426f1df95d2b16b6b85b",
+    "url": "/avalon/static/media/devil.1f69d8ac.png"
+  },
+  {
+    "revision": "2301d6e4b44cd0d50f94f96d67b234ac",
+    "url": "/avalon/static/media/loyalServant.2301d6e4.png"
+  },
+  {
+    "revision": "93d86e3d61903f928012d57522c5fc10",
+    "url": "/avalon/static/media/merlin.93d86e3d.png"
+  },
+  {
+    "revision": "29c5ab413b48332dac9fb1d53d906936",
+    "url": "/avalon/static/media/morgana.29c5ab41.png"
+  },
+  {
+    "revision": "72a9da8eeef192cde8a6021355bd4f78",
+    "url": "/avalon/static/media/percival.72a9da8e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/avalon/precache-manifest.febf5ff21bf109496bb060de9deeaecd.js"
+  "/avalon/precache-manifest.9d967b0ac99dc9b9a9ee4c54308e4f0a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/avalon/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
